5581f9b2dffbf0271fc1e929f2b2c9782a8f56ec
animeiceza <122235710+animeiceza@users.noreply.github.com>
2023-01-09 05:06:03 +0700

    Add files via upload

---
+++ [พากย์_ไทย] Squid Game (2021).php
@@ -0,0 +1,56 @@
+{ 
+name: "[พากย์​ไทย] Squid Game (2021) สควิดเกม เล่นลุ้นตาย [พากย์​ไทย]",
+author: "ตอนที่ 1-9 จบ",
+image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcCDI7JVH7-lznAIAZcSifdj-D5UxfKjB6rQ&usqp=CAU",
+url: "https://pastebin.com/raw/4t79X82f",  
+stations: [
+ 
+{
+name: "ตอนที่ 1",
+image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcCDI7JVH7-lznAIAZcSifdj-D5UxfKjB6rQ&usqp=CAU",
+url: "https://xxx.77player.xyz:443/ioshls/379c58d4ed6b409a0fef2bbf07fb8d74/379c58d4ed6b409a0fef2bbf07fb8d74.m3u8"
+},
+ 
+{
+name: "ตอนที่ 2",
+image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcCDI7JVH7-lznAIAZcSifdj-D5UxfKjB6rQ&usqp=CAU",
+url: "https://xxx.77player.xyz:443/ioshls/6e578cd5cc671ec70690c705a70d1a54/6e578cd5cc671ec70690c705a70d1a54.m3u8"
+},
+{
+name: "ตอนที่ 3",
+image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcCDI7JVH7-lznAIAZcSifdj-D5UxfKjB6rQ&usqp=CAU",
+url: "https://xxx.77player.xyz:443/ioshls/c73512ba1b00c919e29a31bb973f5ac3/c73512ba1b00c919e29a31bb973f5ac3.m3u8"
+},
+{
+name: "ตอนที่ 4",
+image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcCDI7JVH7-lznAIAZcSifdj-D5UxfKjB6rQ&usqp=CAU",
+url: "https://xxx.77player.xyz:443/ioshls/72a4c2068f91eb56b3a0710d1de5dc4d/72a4c2068f91eb56b3a0710d1de5dc4d.m3u8"
+},
+{
+name: "ตอนที่ 5",
+image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcCDI7JVH7-lznAIAZcSifdj-D5UxfKjB6rQ&usqp=CAU",
+url: "https://xxx.77player.xyz:443/ioshls/076191dfa6e757663d22788b8ae476b1/076191dfa6e757663d22788b8ae476b1.m3u8"
+},
+{
+name: "ตอนที่ 6",
+image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcCDI7JVH7-lznAIAZcSifdj-D5UxfKjB6rQ&usqp=CAU",
+url: "https://xxx.77player.xyz:443/ioshls/b53d9837b5f2efbdc9af965d4f2d9d8f/b53d9837b5f2efbdc9af965d4f2d9d8f.m3u8"
+},
+{
+name: "ตอนที่ 7",
+image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcCDI7JVH7-lznAIAZcSifdj-D5UxfKjB6rQ&usqp=CAU",
+url: "https://xxx.77player.xyz:443/ioshls/372948c15d89db370ba26bf3a4eb69a2/372948c15d89db370ba26bf3a4eb69a2.m3u8"
+},
+{
+name: "ตอนที่ 8",
+image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcCDI7JVH7-lznAIAZcSifdj-D5UxfKjB6rQ&usqp=CAU",
+url: "https://xxx.77player.xyz:443/ioshls/511c3958ea881718eb3e8abb701e2784/511c3958ea881718eb3e8abb701e2784.m3u8"
+},
+{
+name: "ตอนที่ 9",
+image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcCDI7JVH7-lznAIAZcSifdj-D5UxfKjB6rQ&usqp=CAU",
+url: "https://xxx.77player.xyz:443/ioshls/0cd5971d0b4bd1e356d77cce8454f47d/0cd5971d0b4bd1e356d77cce8454f47d.m3u8"
+},
+
+]
+}
